8262fbe91cff0b2ea5d70340e8ff0991ac86a268
matwasdev <matwasdev@gmail.com>
2025-06-04 17:13:23 +0200

    validation added

--- kolokwium2/DTOs/CreateWashingMachineRequestDto.cs
+++ kolokwium2/DTOs/CreateWashingMachineRequestDto.cs
@@ -1,18 +1,32 @@
-﻿namespace kolokwium2.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace kolokwium2.DTOs;
 
 public class CreateWashingMachineRequestDto
 {
+    [Required]
     public WashingMachineCreateDto WashingMachine { get; set; }
+    
+    [Required]
     public List<AvailableProgramDto> AvailablePrograms { get; set; }
     
 }
 public class WashingMachineCreateDto{
+    
+    [Required]
     public double MaxWeight { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
     public string SerialNumber { get; set; }
 }
 
 public class AvailableProgramDto
 {
+    [Required]
+    [MaxLength(50)]
     public string ProgramName { get; set; }
+    
+    [Required]
     public double Price { get; set; }
 }
